a05b8d8cc410d1695ffe1b05124cbe555343c27c
Alex R Baker <52913502+alexbaker2002@users.noreply.github.com>
2024-05-01 20:50:31 -0500

    Init with Template Resources

---
+++ pizzaperks/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿namespace pizzaperks.Models
+{
+    public class Product
+    {
+
+
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+
+
+
+
+
+    }
+}
--- pizzaperks/Program.cs
+++ pizzaperks/Program.cs
@@ -1,18 +1,15 @@
 using Microsoft.AspNetCore.Identity;
-using pizzaperks.Data;
 using Microsoft.EntityFrameworkCore;
+using pizzaperks.Data;
 
 var builder = WebApplication.CreateBuilder(args);
-var connectionString = builder.Configuration.GetConnectionString("ApplicationDbContextConnection") ?? throw new InvalidOperationException("Connection string 'ApplicationDbContextConnection' not found.");
-
-builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
 
 // Add services to the container.
-//var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-//builder.Services.AddDbContext<ApplicationDbContext>(options =>
-//    options.UseSqlServer(connectionString));
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
 
-//builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
